3fcc14d0cf91d35ab3b0016308be34b82533289a
U-AD4\jingli <jingli@cv0006081-n0.ad4.ad.alcatel.com>
2017-04-13 14:22:59 +0800

    make script can run on GUI or cmd line.

--- scfvalidation/ScfValidation.py
+++ scfvalidation/ScfValidation.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import collections
 import re
 import os
+import sys
 import tkinter
 import tkinter.ttk
 import tkinter.filedialog
@@ -13,7 +14,6 @@ Param = collections.namedtuple("Param", "name mandatory type min max values")
 Mo = collections.namedtuple("Mo", "minoccurs maxoccurs params")
 modict = {}
 paramdict = {}
-global textview
 
 def ParseNIDD(niddpath):
 	modict.clear()
@@ -230,9 +230,10 @@ def ValidateSCF(scfpath):
 		try:
 			etscf = ET.parse(filename)
 			root = etscf.getroot()
-			WriteLog("{}\n".format("#" * 50))
-			WriteLog("{} validattion begin\n".format(filename))
-			WriteLog("{}\n".format("#" * 50))
+			logfile = open("{}.log".format(filename), "w", encoding = "utf8")
+			WriteLog(logfile, "{}\n".format("#" * 50))
+			WriteLog(logfile, "{} validattion begin\n".format(filename))
+			print("{} validattion begin".format(filename))
 		except FileNotFoundError as err:
 			print(err)
 			return
@@ -253,16 +254,18 @@ def ValidateSCF(scfpath):
 				distname = mo.attrib["distName"]
 				if distname.find("/FTM-") != -1 or classname.find("_R") != -1:
 					continue
-				result = ValidateInstanceId(classname, distname)
+				result = ValidateInstanceId(logfile, classname, distname)
 				if result is True:
 					for param in mo:
-						ValidateParamValue(classname, distname, param.attrib["name"], param)
+						ValidateParamValue(logfile, classname, distname, param.attrib["name"], param)
 
-				ValidateMandatoryParams(classname, distname, mo)
-		ValidateMandatoryClass(MOs)
-	WriteLog("{} validation end\n".format(filename))
+				ValidateMandatoryParams(logfile, classname, distname, mo)
+		ValidateMandatoryClass(logfile, MOs)
+		WriteLog(logfile, "{} validation end\n".format(filename))
+		print("{} validattion end".format(filename))
+		logfile.close()
 
-def ValidateMandatoryParams(clsname, distname, mo):
+def ValidateMandatoryParams(logfile, clsname, distname, mo):
 	try:
 		fullclsname, number = re.subn("-[0-9]*", "", distname)
 		for paramname in modict[fullclsname].params:
@@ -276,7 +279,7 @@ def ValidateMandatoryParams(clsname, distname, mo):
 						found = True
 						break
 				if found is False:
-					WriteLog("Mandatory parameter {}-{} is missing!\n".format(distname, paramname))
+					WriteLog(logfile, "Mandatory parameter {}-{} is missing!\n".format(distname, paramname))
 			if paramdict[key].type != "list":
 				continue
 			for value in paramdict[key].values:
@@ -290,11 +293,11 @@ def ValidateMandatoryParams(clsname, distname, mo):
 										found = True
 										break
 							if found is False:
-								WriteLog("Mandatory parameter {}-{}-{} is missing!\n".format(distname, paramname, value.name))
+								WriteLog(logfile, "Mandatory parameter {}-{}-{} is missing!\n".format(distname, paramname, value.name))
 	except (KeyError, AttributeError) as err:
 		print(err)
 
-def ValidateMandatoryClass(objlist):
+def ValidateMandatoryClass(logfile, objlist):
 	for key in modict:
 		if key.find("/FTM") != -1:
 			continue
@@ -316,22 +319,18 @@ def ValidateMandatoryClass(objlist):
 			parentcounts = GetCounts(parentname, objlist, True)
 		counts = GetCounts(name, objlist)
 
-		try:
-			for parentdn in parentcounts:
-				for dn in counts:
-					if dn[: dn.rfind("/")] == parentdn:
-						count = counts[dn]
-						if not modict[key].minoccurs <= count <= modict[key].maxoccurs:
-							WriteLog("{} object nubmer {} not in range [{}-{}]\n".format(dn, count, modict[key].minoccurs, modict[key].maxoccurs))
-						counts.pop(dn)
-						break
+		for parentdn in parentcounts:
 			for dn in counts:
-				fullname, num = re.subn("-[0-9]*", "", dn)
-				if fullname == key and parentname is not None:
-					WriteLog("{} parent object is missing!\n".format(dn))
-		except AttributeError as err:
-			print(err)
-			print(dn, parentdn)
+				if dn[: dn.rfind("/")] == parentdn:
+					count = counts[dn]
+					if not modict[key].minoccurs <= count <= modict[key].maxoccurs:
+						WriteLog(logfile, "{} object nubmer {} not in range [{}-{}]\n".format(dn, count, modict[key].minoccurs, modict[key].maxoccurs))
+					counts.pop(dn)
+					break
+		for dn in counts:
+			fullname, num = re.subn("-[0-9]*", "", dn)
+			if fullname == key and parentname is not None:
+				WriteLog(logfile, "{} parent object is missing!\n".format(dn))
 
 def GetCounts(name, objs, isparent = False):
 	counts = {}
@@ -348,20 +347,20 @@ def GetCounts(name, objs, isparent = False):
 				counts[distname] = 1
 	return counts
 
-def ValidateInstanceId(clsname, distname):
+def ValidateInstanceId(logfile, clsname, distname):
 	result = True
 	instid = int(distname[distname.rindex("-") + 1 :])
 	key = "{}-{}".format(clsname, "instanceid")
 	try:
 		if not paramdict[key].min <= instid <= paramdict[key].max:
-			WriteLog("{} instance id {} not in range [{}-{}]!\n".format(distname, instid, paramdict[key].min, paramdict[key].max))
+			WriteLog(logfile, "{} instance id {} not in range [{}-{}]!\n".format(distname, instid, paramdict[key].min, paramdict[key].max))
 			result = False
 	except KeyError:
-		WriteLog("Unknown object {}!\n".format(distname))
+		WriteLog(logfile, "Unknown object {}!\n".format(distname))
 		result = False
 	return result
 
-def ValidateParamValue(classname, distname, name, param, islist = False):
+def ValidateParamValue(logfile, classname, distname, name, param, islist = False):
 	paramname = param.attrib["name"]
 	key = "{}-{}".format(classname, name)
 	try:
@@ -392,110 +391,115 @@ def ValidateParamValue(classname, distname, name, param, islist = False):
 			for value in values:
 				if paramtype == "decimal":
 					if not  minval <= int(value) <= maxval and value not in special:
-						WriteLog("{} value {} exceeds range [{}-{}]!\n".format(paramdn, value, minval, maxval))
+						WriteLog(logfile, "{} value {} exceeds range [{}-{}]!\n".format(paramdn, value, minval, maxval))
 				elif paramtype == "string":
 					if not minval <= len(value) <= maxval and len(value) not in special:
-						WriteLog("{} value length {} exceeds range [{}-{}]!\n".format(paramdn, len(param.text), minval, maxval))
+						WriteLog(logfile, "{} value length {} exceeds range [{}-{}]!\n".format(paramdn, len(param.text), minval, maxval))
 		elif paramtype == "boolean":
 			if param.text not in ("true", "false"):
-				WriteLog("{} value {} is not 'true' or 'false'!\n".format(paramdn, param.text))
+				WriteLog(logfile, "{} value {} is not 'true' or 'false'!\n".format(paramdn, param.text))
 		elif paramtype == "enumeration":
 			if param.text not in special:
-				WriteLog("{} value {} is not in range {}!\n".format(paramdn, param.text, special))
+				WriteLog(logfile, "{} value {} is not in range {}!\n".format(paramdn, param.text, special))
 		elif paramtype == "bit":
 			if (int(param.text) & 0xFFFF) > maxval:
-				WriteLog("{} value {} is not in bit range {}(0-{})!\n".format(paramdn, param.text, special, maxval))
+				WriteLog(logfile, "{} value {} is not in bit range {}(0-{})!\n".format(paramdn, param.text, special, maxval))
 		elif paramtype == "list":
 			for item in param.findall("{raml21.xsd}item"):
 				for listparam in item:
-					ValidateParamValue(classname, distname, paramname, listparam, True)
+					ValidateParamValue(logfile, classname, distname, paramname, listparam, True)
 	except KeyError:
-		WriteLog("Unknown parameter {}-{}\n".format(distname, name))
+		WriteLog(logfile, "Unknown parameter {}-{}\n".format(distname, name))
 		#print("{} does not exist".format(key))
 		return
 
-def SelectDir(var):
-	#filename = tkinter.filedialog.askopenfilename(filetypes=[("xml file", "*.xml")])
-	dirname = tkinter.filedialog.askdirectory()
-	#filename = "{}{}".format(filename, "/log.txt")
-	var.set(dirname)
+def WriteLog(logfile, text):
+	logfile.write(text)
+	logfile.write("-" * 50 + "\n")
 
-def OnValidate():
-	global textview
-	textview.delete(1.0, "end")
-
-	if niddpath.get() == "" or scfpath.get() == "":
-		print("SCF or file is not selected!!!")
-		tkinter.messagebox.showinfo(title = "Information", message = "NIDD or SCF directory is not specified!")
-		return
-
-	result = ParseNIDD(niddpath.get())
-	
-	#for mo in modict:
-		#print("{}:{}".format(mo, modict[mo]))
-	#for param in paramdict:
-		#print("{}:{}".format(param, paramdict[param]))
-	
-	if result is True:
-		ValidateSCF(scfpath.get())
-
-	if result is True:
-		message = "SCF validation complete!"
-	else:
-		message = "SCF validation failure!"
-	tkinter.messagebox.showinfo(title = "Information", message = message)
-
+'''
 def WriteLog(text):
 	global textview
 	textview.insert("current", text)
 	textview.insert("current", "-" * 50 + "\n")
-
-def ExportLog():
-	global textview
-	filename = 	tkinter.filedialog.asksaveasfilename()
-	logfile = open(filename, "w", encoding = "utf8")
-	logfile.write(textview.get(0.0, "end"))
-	logfile.close()
+'''
 
 if __name__=='__main__':
-	global textview
 
-	root = tkinter.Tk()
-	root.title("SCF Validator")
-	root.columnconfigure(1, weight = 1)
+	def SelectDir(var):
+		#filename = tkinter.filedialog.askopenfilename(filetypes=[("xml file", "*.xml")])
+		dirname = tkinter.filedialog.askdirectory()
+		#filename = "{}{}".format(filename, "/log.txt")
+		var.set(dirname)
 
-	niddpath = tkinter.StringVar()
-	tkinter.ttk.Button(root, text = "NIDD", command = lambda:SelectDir(niddpath)).grid(row = 0, column = 0, padx = 4, pady = 4)
-	tkinter.ttk.Entry(root, textvariable = niddpath, width = 30).grid(row = 0, column  = 1, padx = 4, pady = 4, sticky = "wesn")
-	
-	scfpath = tkinter.StringVar()
-	tkinter.ttk.Button(root, text = "SCF", command = lambda:SelectDir(scfpath)).grid(row = 1, column = 0, padx = 4, pady = 4)
-	tkinter.ttk.Entry(root, textvariable = scfpath, width = 30).grid(row = 1, column  = 1, padx = 4, pady = 4, sticky = "wesn")
-	
-	#mrbtsVar= tkinter.StringVar()
-	#tkinter.ttk.Button(root, text = "MRBTS", command = lambda:OpenFile(mrbtsVar)).grid(row = 0, column = 0, padx = 4, pady = 4)
-	#tkinter.ttk.Entry(root, textvariable = mrbtsVar, width = 30).grid(row = 0, column  = 1, padx = 4, pady = 4, sticky = "wesn")
+	def OnValidate():
+		global textview
+		textview.delete(1.0, "end")
 
-	#eqmVar = tkinter.StringVar()
-	#tkinter.ttk.Button(root, text = "EQM", command = lambda:OpenFile(eqmVar)).grid(row = 1, column = 0, padx = 4, pady = 4)
-	#tkinter.ttk.Entry(root, textvariable = eqmVar, width = 30).grid(row = 1, column = 1, padx = 4, pady = 4, sticky = "wesn")
+		if niddpath.get() == "" or scfpath.get() == "":
+			print("SCF or file is not selected!!!")
+			tkinter.messagebox.showinfo(title = "Information", message = "NIDD or SCF directory is not specified!")
+			return
 
-	#mnlVar = tkinter.StringVar()
-	#tkinter.ttk.Button(root, text = "MNL", command = lambda:OpenFile(mnlVar)).grid(row = 2, column = 0, padx = 4, pady = 4)
-	#tkinter.ttk.Entry(root, textvariable = mnlVar, width = 30).grid(row = 2, column = 1, padx = 4, pady = 4, sticky = "wesn")
+		result = ParseNIDD(niddpath.get())
+		
+		#for mo in modict:
+			#print("{}:{}".format(mo, modict[mo]))
+		#for param in paramdict:
+			#print("{}:{}".format(param, paramdict[param]))
+		
+		if result is True:
+			ValidateSCF(scfpath.get())
 
-	#radioVar = tkinter.StringVar()
-	#tkinter.ttk.Button(root, text = "RADIO", command = lambda:OpenFile(radioVar)).grid(row = 3, column = 0, padx = 4, pady = 4)
-	#tkinter.ttk.Entry(root, textvariable = radioVar, width = 30).grid(row = 3, column = 1, padx = 4, pady = 4, sticky = "wesn")
+		if result is True:
+			message = "SCF validation complete!"
+		else:
+			message = "SCF validation failure!"
+		tkinter.messagebox.showinfo(title = "Information", message = message)
+	'''
+	def ExportLog():
+		global textview
+		filename = 	tkinter.filedialog.asksaveasfilename()
+		logfile = open(filename, "w", encoding = "utf8")
+		logfile.write(textview.get(0.0, "end"))
+		logfile.close()
+	'''
+	def RunGuiMode():
+		root = tkinter.Tk()
+		root.title("SCF Validator")
+		root.columnconfigure(1, weight = 1)
 
-	#scfVar = tkinter.StringVar()
-	#tkinter.ttk.Button(root, text = "SCF", command = lambda:OpenFile(scfVar)).grid(row = 4, column = 0, padx = 4, pady = 4)
-	#tkinter.ttk.Entry(root, textvariable = scfVar, width = 30).grid(row = 4, column = 1, padx = 4, pady = 4, sticky = "wesn")
+		niddpath = tkinter.StringVar()
+		tkinter.ttk.Button(root, text = "NIDD", command = lambda:SelectDir(niddpath)).grid(row = 0, column = 0, padx = 4, pady = 4)
+		tkinter.ttk.Entry(root, textvariable = niddpath, width = 30).grid(row = 0, column  = 1, padx = 4, pady = 4, sticky = "wesn")
+		
+		scfpath = tkinter.StringVar()
+		tkinter.ttk.Button(root, text = "SCF", command = lambda:SelectDir(scfpath)).grid(row = 1, column = 0, padx = 4, pady = 4)
+		tkinter.ttk.Entry(root, textvariable = scfpath, width = 30).grid(row = 1, column  = 1, padx = 4, pady = 4, sticky = "wesn")
+		
+		tkinter.ttk.Button(root, text = "Validate", command = OnValidate).grid(row = 5, column = 0,  columnspan = 1, padx = 4, pady = 4)
+		#tkinter.ttk.Button(root, text = "Export LOG", command = ExportLog).grid(row = 6, column = 0, padx = 4, pady = 4)
 
-	tkinter.ttk.Button(root, text = "Validate", command = OnValidate).grid(row = 5, column = 0,  columnspan = 1, padx = 4, pady = 4)
-	#tkinter.ttk.Button(root, text = "Export LOG", command = ExportLog).grid(row = 6, column = 0, padx = 4, pady = 4)
+		textview = tkinter.scrolledtext.ScrolledText(root, font = ('Arial', '12', 'normal'), wrap = "word")
+		textview.grid(row = 6, column = 0, columnspan = 2, sticky = "wesn")
+		
+		root.mainloop()
 
-	textview = tkinter.scrolledtext.ScrolledText(root, font = ('Arial', '12', 'normal'), wrap = "word")
-	textview.grid(row = 6, column = 0, columnspan = 2, sticky = "wesn")
-	
-	root.mainloop()
+	def RunCmdMode(param):
+		if len(param) < 3 or param[1] in {"-h", "--help"}:
+			print("usage:{} niddpath scfpath".format(sys.argv[0]))
+			sys.exit()
+
+		if ParseNIDD(param[1]) is True:
+			ValidateSCF(param[2])
+			print("All SCF validation complete!!!")
+		else:
+			print("NIDD parse failure!!!")
+
+	def main():
+		if len(sys.argv) == 1:
+			RunGuiMode()
+		else:
+			RunCmdMode(sys.argv)
+
+	main()
